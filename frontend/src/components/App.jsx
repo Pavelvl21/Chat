98f49711bc98e24da7165f022534f5df3b0056f5
Pavel <p.yudenko@gmail.com>
2022-12-26 23:00:46 +0300

    add login page, add error page

--- frontend/src/components/App.jsx
+++ frontend/src/components/App.jsx
@@ -1,3 +1,22 @@
-const App = () => <div>Home page</div>;
+import React from 'react';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <div>Home page</div>,
+    errorElement: <div>Error page</div>,
+  },
+  {
+    path: '/login',
+    element: <div>Login page</div>,
+  }
+]);
+
+const App = () => {
+  return(
+    <RouterProvider router={router} />
+  );
+}
 
 export default App;
