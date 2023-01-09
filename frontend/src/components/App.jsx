859007e9995190c243e57cec16187b3ad30e68a8
Pavel <p.yudenko@gmail.com>
2023-01-09 15:16:57 +0300

    step3: add private page

--- frontend/src/components/App.jsx
+++ frontend/src/components/App.jsx
@@ -1,21 +1,58 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import React, { useState, useMemo } from 'react';
+
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Outlet,
+  Navigate,
+} from 'react-router-dom';
 import NotFoundPage from './NotFoundPage.jsx';
 import LoginPage from './LoginPage.jsx';
 import Navbar from './Navbar';
 
 import '../assets/app.scss';
+import AuthContext from '../contexts/index.jsx';
+import useAuth from '../hooks/index.js';
+
+const AuthProvider = ({ children }) => {
+  const currentUser = JSON.parse(localStorage.getItem('user'));
+  const [user, setUser] = useState(currentUser ? { username: currentUser.username } : null);
+  const logIn = (userData) => {
+    localStorage.setItem('user', JSON.stringify(userData));
+    setUser({ username: userData.username });
+  };
+
+  const auth = useMemo(() => ({ logIn, user }), [user]);
+
+  return (
+    <AuthContext.Provider value={auth}>
+      {children}
+    </AuthContext.Provider>
+  );
+};
+
+const PrivateOutlet = () => {
+  const auth = useAuth();
+  console.log(auth);
+  return auth.user ? <Outlet /> : <Navigate to="/login" />;
+};
 
 const App = () => (
-  <Router>
-    <div className="d-flex flex-column vh-100">
-      <Navbar />
-      <Routes>
-        <Route path="/login" element={<LoginPage />} />
-        <Route path="/" element={<div>Hello, piplz</div>} />
-        <Route path="*" element={<NotFoundPage />} />
-      </Routes>
-    </div>
-  </Router>
+  <AuthProvider>
+    <Router>
+      <div className="d-flex flex-column vh-100">
+        <Navbar />
+        <Routes>
+          <Route path="/login" element={<LoginPage />} />
+          <Route path="/" element={<PrivateOutlet />}>
+            <Route path="" element={<h1>You did it, mthfckr!</h1>} />
+          </Route>
+          <Route path="*" element={<NotFoundPage />} />
+        </Routes>
+      </div>
+    </Router>
+  </AuthProvider>
 );
 
 export default App;
--- frontend/src/components/LoginPage.jsx
+++ frontend/src/components/LoginPage.jsx
@@ -1,8 +1,12 @@
 /* eslint-disable react/jsx-props-no-spreading */
-import { Link } from 'react-router-dom';
+import React from 'react';
+import axios from 'axios';
+import { Link, useLocation, useNavigate } from 'react-router-dom';
 import { Formik, Form, useField } from 'formik';
 import * as Yup from 'yup';
 import logo from '../assets/LoginPage.png';
+
+import useAuth from '../hooks/index.js';
 
 const TextInput = ({ label, ...props }) => {
   const [field, meta] = useField(props);
@@ -17,57 +21,63 @@ const TextInput = ({ label, ...props }) => {
   );
 };
 
-const LoginPage = () => (
-  <Formik
-    initialValues={{ userName: '', password: '' }}
-    validationSchema={Yup.object().shape({
-      userName: Yup.string().trim().matches(/^[a-z0-9_-]{3,16}$/).required('Required filed'),
-      password: Yup.string().trim().required('Required filed'),
-    })}
-    onSubmit={(values, actions) => {
-      setTimeout(() => {
-        console.log(JSON.stringify(values, null, 2));
-        actions.setSubmitting(false);
-      }, 400);
-    }}
-  >
-    <div className="container-fluid h-100">
-      <div className="row justify-content-center align-content-center h-100">
-        <div className="col-12 col-md-8 col-xxl-6">
-          <div className="card shadow-sm">
-            <div className="card-body row p-5">
-              <div className="col-12 col-md-6 d-flex align-items-center justify-content-center">
-                <img src={logo} alt="img" height="200" />
+const LoginPage = () => {
+  const location = useLocation();
+  const navigate = useNavigate();
+  const auth = useAuth();
+  return (
+    <Formik
+      initialValues={{ username: '', password: '' }}
+      validationSchema={Yup.object().shape({
+        username: Yup.string().trim().matches(/^[a-z0-9_-]{3,16}$/).required('Required filed'),
+        password: Yup.string().trim().required('Required filed'),
+      })}
+      onSubmit={async (values) => {
+        const response = await axios.post('/api/v1/login', values);
+        auth.logIn(response.data);
+        const { from } = location.state || { from: { pathname: '/' } };
+        navigate(from);
+        return response;
+      }}
+    >
+      <div className="container-fluid h-100">
+        <div className="row justify-content-center align-content-center h-100">
+          <div className="col-12 col-md-8 col-xxl-6">
+            <div className="card shadow-sm">
+              <div className="card-body row p-5">
+                <div className="col-12 col-md-6 d-flex align-items-center justify-content-center">
+                  <img src={logo} alt="img" height="200" />
+                </div>
+                <Form className="col-12 col-md-6 mt-3 mt-mb-0">
+                  <h1 className="text-center mb-4">Войти</h1>
+                  <TextInput
+                    label="Ваш ник"
+                    name="username"
+                    type="text"
+                    placeholder="Ваш ник"
+                  />
+                  <TextInput
+                    label="Пароль"
+                    name="password"
+                    type="password"
+                    placeholder="Пароль"
+                  />
+                  <button type="submit" className="btn btn-outline-primary w-100 mt-2 mb-3">Войти</button>
+                </Form>
               </div>
-              <Form className="col-12 col-md-6 mt-3 mt-mb-0">
-                <h1 className="text-center mb-4">Войти</h1>
-                <TextInput
-                  label="Ваш ник"
-                  name="userName"
-                  type="text"
-                  placeholder="Ваш ник"
-                />
-                <TextInput
-                  label="Пароль"
-                  name="password"
-                  type="password"
-                  placeholder="Пароль"
-                />
-                <button type="submit" className="btn btn-outline-primary w-100 mt-2 mb-3">Войти</button>
-              </Form>
-            </div>
-            <div className="card-footer p-4">
-              <div className="text-center">
-                <span>Нет аккаунта?</span>
-                {' '}
-                <Link to="/login">Регистрация</Link>
+              <div className="card-footer p-4">
+                <div className="text-center">
+                  <span>Нет аккаунта?</span>
+                  {' '}
+                  <Link to="/login">Регистрация</Link>
+                </div>
               </div>
             </div>
           </div>
         </div>
       </div>
-    </div>
-  </Formik>
-);
+    </Formik>
+  );
+};
 
 export default LoginPage;
