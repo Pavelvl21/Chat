0d1defeb7b3c892753b06c59c820481e148f4665
Pavel <p.yudenko@gmail.com>
2023-02-21 22:19:00 +0300

    add provider

--- frontend/src/init.jsx
+++ frontend/src/init.jsx
@@ -1,7 +1,12 @@
+import { Provider } from 'react-redux';
 import App from './components/App.jsx';
 
+import store from './slices/index.js';
+
 const init = () => (
-  <App />
+  <Provider store={store}>
+    <App />
+  </Provider>
 );
 
 export default init;
