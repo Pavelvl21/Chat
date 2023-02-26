e99f82af1aaff08e38bf98a16a8382d095957b3e
Pavel <p.yudenko@gmail.com>
2023-02-26 20:32:52 +0300

    feat: add default select to the current channel

--- frontend/src/components/ChannelsBox.jsx
+++ frontend/src/components/ChannelsBox.jsx
@@ -2,28 +2,33 @@ import React from 'react';
 import { useSelector } from 'react-redux';
 import { Button } from 'react-bootstrap';
 
-const Channel = ({ channel }) => (
-  <li key={channel.id} className="nav-item w-100">
-    <Button
-      type="button"
-      variant="secondary"
-      key={channel.id}
-      className="w-100 rounded-0 text-start"
-    >
-      <span className="me-1">#</span>
-      {channel.name}
-    </Button>
-  </li>
-);
+const Channel = (props) => {
+  const { channel, isCurrent } = props;
+  const variant = isCurrent ? 'secondary' : null;
+  return (
+    <li key={channel.id} className="nav-item w-100">
+      <Button
+        type="button"
+        variant={variant}
+        key={channel.id}
+        className="w-100 rounded-0 text-start"
+      >
+        <span className="me-1">#</span>
+        {channel.name}
+      </Button>
+    </li>
+  );
+};
 
 const ChannelsBox = () => {
-  const { channels } = useSelector((state) => state.channelsData);
+  const { channels, currentChannelId } = useSelector((state) => state.channelsData);
   return (
     <ul className="nav flex-column nav-pills nav-fill px-2">
       {channels.map((channel) => (
         <Channel
           key={channel.id}
           channel={channel}
+          isCurrent={channel.id === currentChannelId}
         />
       ))}
     </ul>
