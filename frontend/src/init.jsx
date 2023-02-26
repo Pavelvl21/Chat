b29aa9bb70865d9db191fb076b8ceb791db266b6
Pavel <p.yudenko@gmail.com>
2023-02-26 16:43:17 +0300

    feat: add Api provider, add cocket.on

--- frontend/src/init.jsx
+++ frontend/src/init.jsx
@@ -1,12 +1,24 @@
 import { Provider } from 'react-redux';
 import App from './components/App.jsx';
 
-import store from './slices/index.js';
+import store, { actions } from './slices/index.js';
+import contexts from './contexts/index.js';
 
-const init = () => (
-  <Provider store={store}>
-    <App />
-  </Provider>
-);
+const init = async (socket) => {
+  const { ApiContext } = contexts;
+  const Api = {
+    sendMessage: (...args) => socket.emit('newMessage', ...args),
+  };
+  socket.on('newMessage', (payload) => {
+    store.dispatch(actions.addMessage({ message: payload }));
+  });
+  return (
+    <Provider store={store}>
+      <ApiContext.Provider value={Api}>
+        <App />
+      </ApiContext.Provider>
+    </Provider>
+  );
+};
 
 export default init;
