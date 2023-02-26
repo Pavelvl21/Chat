2d591bad0e3b2abecc5f72e173c2991b68eb607d
Pavel <p.yudenko@gmail.com>
2023-02-26 21:28:19 +0300

    feat: add the choose channel handler

--- frontend/src/components/ChannelsBox.jsx
+++ frontend/src/components/ChannelsBox.jsx
@@ -1,9 +1,14 @@
 import React from 'react';
-import { useSelector } from 'react-redux';
+import { useSelector, useDispatch } from 'react-redux';
 import { Button } from 'react-bootstrap';
+import { actions } from '../slices/index.js';
 
 const Channel = (props) => {
-  const { channel, isCurrent } = props;
+  const {
+    channel,
+    isCurrent,
+    handleChooseChannel,
+  } = props;
   const variant = isCurrent ? 'secondary' : null;
   return (
     <li key={channel.id} className="nav-item w-100">
@@ -12,6 +17,7 @@ const Channel = (props) => {
         variant={variant}
         key={channel.id}
         className="w-100 rounded-0 text-start"
+        onClick={handleChooseChannel}
       >
         <span className="me-1">#</span>
         {channel.name}
@@ -21,7 +27,11 @@ const Channel = (props) => {
 };
 
 const ChannelsBox = () => {
+  const dispatch = useDispatch();
   const { channels, currentChannelId } = useSelector((state) => state.channelsData);
+  const handleChooseChannel = (channelId) => () => {
+    dispatch(actions.setCurrentChannel({ channelId }));
+  };
   return (
     <ul className="nav flex-column nav-pills nav-fill px-2">
       {channels.map((channel) => (
@@ -29,6 +39,7 @@ const ChannelsBox = () => {
           key={channel.id}
           channel={channel}
           isCurrent={channel.id === currentChannelId}
+          handleChooseChannel={handleChooseChannel(channel.id)}
         />
       ))}
     </ul>
