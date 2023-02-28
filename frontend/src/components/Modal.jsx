5f8b994b6a71435f5c43b31b2e221b682dd1243f
Pavel <p.yudenko@gmail.com>
2023-02-28 20:43:25 +0300

    fix: add validation schema to modal form

--- frontend/src/components/Modal.jsx
+++ frontend/src/components/Modal.jsx
@@ -2,28 +2,45 @@ import React from 'react';
 import { Modal as BtsModal, Button, Form } from 'react-bootstrap';
 import { useSelector, useDispatch } from 'react-redux';
 import { useFormik } from 'formik';
+import * as yup from 'yup';
 import { actions } from '../slices/index.js';
 import useHook from '../hooks/index.js';
+
+const getChannelsNames = ({ channelsData: { channels } }) => channels
+  .map(({ name }) => name);
 
 const Modal = () => {
   const dispatch = useDispatch();
   const { isOpened } = useSelector((state) => state.modal);
+  const channels = useSelector(getChannelsNames);
   const handleClose = () => dispatch(actions.closeModal());
   const { useApi } = useHook;
   const api = useApi();
 
+  const validationSchema = yup.object().shape({
+    name: yup
+      .string()
+      .trim()
+      .matches(/^[a-z0-9_-]{3,16}$/)
+      .notOneOf(channels)
+      .required('Required filed'),
+  });
+
   const formik = useFormik({
     initialValues: {
-      channelName: '',
+      name: '',
     },
-    onSubmit: async ({ channelName }) => {
-      const channel = { name: channelName };
+    validationSchema,
+    onSubmit: async ({ name }) => {
+      const channel = { name };
       try {
         const data = await api.createChannel(channel);
         dispatch(actions.setCurrentChannel({ channelId: data.id }));
         handleClose();
       } catch (error) {
         console.error(error);
+        formik.setSubmitting(false);
+        formik.resetForm();
       }
     },
   });
@@ -41,10 +58,10 @@ const Modal = () => {
               disabled={formik.isSubmitting}
               onChange={formik.handleChange}
               value={formik.values.name}
-              name="channelName"
-              id="channelName"
+              name="name"
+              id="name"
             />
-            <label className="visually-hidden" htmlFor="channelName">{formik.channelName}</label>
+            <label className="visually-hidden" htmlFor="name">{formik.name}</label>
             <div className="d-flex justify-content-end">
               <Button
                 className="me-2"
