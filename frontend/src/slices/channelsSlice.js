00f44844fe688e55e9072cb1d94bbd1469a7ae28
Pavel <p.yudenko@gmail.com>
2023-03-01 23:59:48 +0300

    feat: add reducers

--- frontend/src/slices/channelsSlice.js
+++ frontend/src/slices/channelsSlice.js
@@ -1,6 +1,6 @@
 /* eslint-disable no-param-reassign */
-
 import { createSlice } from '@reduxjs/toolkit';
+import remove from 'lodash/remove.js';
 
 const initialState = {
   channels: [],
@@ -23,6 +23,18 @@ const commentsSlice = createSlice({
     addChannel(state, { payload }) {
       const { channel } = payload;
       state.channels.push(channel);
+    },
+    removeChannel(state, { payload }) {
+      const { channelId } = payload;
+      remove(state.channels, (({ id }) => id === channelId));
+      if (channelId === state.currentChannelId) {
+        state.currentChannelId = 1;
+      }
+    },
+    renameChannel(state, { payload }) {
+      const { channelId, channelName } = payload;
+      const channel = state.channels.find(({ id }) => id === channelId);
+      channel.name = channelName;
     },
   },
 });
