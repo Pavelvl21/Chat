450aaee54a699033a59820f28df37004a266511f
Pavel <p.yudenko@gmail.com>
2023-03-03 01:03:13 +0300

    refactor: add essence in state

--- frontend/src/slices/modalSlice.js
+++ frontend/src/slices/modalSlice.js
@@ -4,17 +4,24 @@ import { createSlice } from '@reduxjs/toolkit';
 
 const initialState = {
   isOpened: false,
+  modalType: null,
+  id: null,
 };
 
 const modalSlice = createSlice({
   name: 'modal',
   initialState,
   reducers: {
-    openModal: (state) => {
+    openModal: (state, { payload }) => {
+      const { modalType, id } = payload;
       state.isOpened = true;
+      state.modalType = modalType;
+      state.id = id ?? null;
     },
     closeModal: (state) => {
       state.isOpened = false;
+      state.modalType = null;
+      state.id = null;
     },
   },
 });
