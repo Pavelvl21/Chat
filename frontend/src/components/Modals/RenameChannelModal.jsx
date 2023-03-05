58584e2fc0bb3c076c4eec8cf938dd7ddb86bfd6
Pavel <p.yudenko@gmail.com>
2023-03-05 20:30:28 +0300

    fix: fix input(no select value)

--- frontend/src/components/Modals/RenameChannelModal.jsx
+++ frontend/src/components/Modals/RenameChannelModal.jsx
@@ -13,7 +13,9 @@ const getChannelsNames = ({ channelsData: { channels } }) => channels
 const RenameChannelModal = ({ handleClose }) => {
   const inputRef = useRef();
   useEffect(() => {
-    setTimeout(() => inputRef.current.select());
+    const cb = () => inputRef.current.select();
+    cb();
+    // setTimeout(() => inputRef.current.select());
   }, []);
   const channelsNames = useSelector(getChannelsNames);
   const channelId = useSelector(({ modal }) => modal.id);
