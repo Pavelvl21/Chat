d6624de29a80a84f4984d6acc8d8291f5b18fa69
Pavel <p.yudenko@gmail.com>
2023-03-07 00:30:06 +0300

    refactor: use useFormic

--- frontend/src/components/SignupPage.jsx
+++ frontend/src/components/SignupPage.jsx
@@ -1,115 +1,107 @@
 /* eslint-disable react/jsx-props-no-spreading */
-import React, { useState } from 'react';
+import { useEffect, useRef } from 'react';
 import axios from 'axios';
 import { useNavigate } from 'react-router-dom';
-import { Formik, Form as FormikForm, useField } from 'formik';
-// import * as Yup from 'yup';
+import { useFormik } from 'formik';
+import { Form, Button } from 'react-bootstrap';
+import * as yup from 'yup';
 
 import useHook from '../hooks/index.js';
 import img from '../assets/SignupPage.png';
 
 const { useAuth } = useHook;
 
-// import axios from 'axios';
-// import React, { useEffect, useRef } from 'react';
-// import { useFormik } from 'formik';
-// import { Button, Form } from 'react-bootstrap';
-// import { useNavigate } from 'react-router-dom';
-// import * as yup from 'yup';
-// import useHook from '../hooks/index.js';
-
-// const { useAuth } = useHook;
-const TextInput = ({ label, validationClass, ...props }) => {
-  const [field, meta] = useField(props);
-  return (
-    <>
-      <input className={`form-control ${validationClass}`} required {...field} {...props} />
-      <label htmlFor={props.id || props.name}>{label}</label>
-      {meta.touched && meta.error ? (
-        <div className="error">{meta.error}</div>
-      ) : null}
-    </>
-  );
-};
-
 const SignupPage = () => {
   const navigate = useNavigate();
   const auth = useAuth();
-  const [authFailed, setAuthFailed] = useState('');
+  const formik = useFormik({
+    initialValues: {
+      username: '',
+      password: '',
+      confirmPassword: '',
+    },
+    validationSchema: yup.object().shape({
+      username: yup.string().min(3).max(20).required(),
+      password: yup.string().min(6).required(),
+      confirmPassword: yup.string().oneOf([yup.ref('password')]),
+    }),
+    onSubmit: async (values) => {
+      try {
+        const { data } = await axios.post(
+          '/api/v1/signup',
+          { username: values.username, password: values.password },
+        );
+        auth.logIn(data);
+        navigate('/');
+      } catch (error) {
+        console.error(error.message);
+      }
+    },
+  });
+  const inputRef = useRef();
+  useEffect(() => {
+    inputRef.current.focus();
+  }, []);
+
   return (
-    <Formik
-      initialValues={{
-        username: '',
-        password: '',
-        confirmPassword: '',
-      }}
-      // validationSchema={Yup.object().shape({
-      //   username: Yup.string().trim().matches(/^[a-z0-9_-]{3,16}$/).required('Required filed'),
-      //   password: Yup.string().trim().required('Required filed'),
-      // })}
-      onSubmit={async (values) => {
-        setAuthFailed('');
-        try {
-          const { data } = await axios.post(
-            '/api/v1/signup',
-            { username: values.username, password: values.password },
-          );
-          auth.logIn(data);
-          navigate('/');
-        } catch (error) {
-          setAuthFailed('is-invalid');
-          console.error(error.message);
-        }
-      }}
-    >
-      <div className="container-fluid h-100">
-        <div className="row justify-content-center align-content-center h-100">
-          <div className="col-12 col-md-8 col-xxl-6">
-            <div className="card shadow-sm">
-              <div className="card-body d-flex flex-column flex-md-row justify-content-around align-items-center p-5">
-                <div>
-                  <img
-                    src={img}
-                    alt="Registration"
-                  />
-                </div>
-                <FormikForm className="col-12 col-md-6 mt-3 mt-mb-0">
-                  <h1 className="text-center mb-4">Войти</h1>
-                  <div className="form-floating mb-3">
-                    <TextInput
-                      label="Имя пользователя"
-                      name="username"
-                      type="text"
-                      placeholder="Имя пользователя"
-                      validationClass={authFailed}
-                    />
-                  </div>
-                  <div className="form-floating mb-3">
-                    <TextInput
-                      label="Пароль"
-                      name="password"
-                      type="password"
-                      placeholder="Пароль"
-                      validationClass={authFailed}
-                    />
-                  </div>
-                  <div className="form-floating mb-3">
-                    <TextInput
-                      label="Подтвердите пароль"
-                      name="confirmPassword"
-                      type="password"
-                      placeholder="Подтвердите пароль"
-                      validationClass={authFailed}
-                    />
-                  </div>
-                  <button type="submit" className="btn btn-outline-primary w-100 mt-2 mb-3">Зарегистрироваться</button>
-                </FormikForm>
+    <div className="container-fluid h-100">
+      <div className="row justify-content-center align-content-center h-100">
+        <div className="col-12 col-md-8 col-xxl-6">
+          <div className="card shadow-sm">
+            <div className="card-body d-flex flex-column flex-md-row justify-content-around align-items-center p-5">
+              <div>
+                <img
+                  src={img}
+                  alt="Registration"
+                />
               </div>
+              <Form onSubmit={formik.handleSubmit} className="w-50">
+                <h1 className="text-center mb-4">Регистрация</h1>
+                <Form.Group className="form-floating mb-3">
+                  <Form.Control
+                    type="text"
+                    onChange={formik.handleChange}
+                    value={formik.values.username}
+                    placeholder="Имя пользователя"
+                    name="username"
+                    id="username"
+                    autoComplete="new-username"
+                    required
+                    ref={inputRef}
+                  />
+                  <Form.Label htmlFor="username">Имя пользователя</Form.Label>
+                </Form.Group>
+                <Form.Group className="form-floating mb-3">
+                  <Form.Control
+                    type="password"
+                    onChange={formik.handleChange}
+                    value={formik.values.password}
+                    placeholder="Пароль"
+                    name="password"
+                    autoComplete="new-password"
+                    required
+                  />
+                  <Form.Label htmlFor="password">Пароль</Form.Label>
+                </Form.Group>
+                <Form.Group className="form-floating mb-3">
+                  <Form.Control
+                    type="password"
+                    onChange={formik.handleChange}
+                    value={formik.values.confirmPassword}
+                    placeholder="Подтвердите пароль"
+                    name="confirmPassword"
+                    autoComplete="new-password"
+                    required
+                  />
+                  <Form.Label htmlFor="confirmPassword">Подтвердите пароль</Form.Label>
+                </Form.Group>
+                <Button type="submit" variant="outline-primary" className="w-100">Зарегистрироваться</Button>
+              </Form>
             </div>
           </div>
         </div>
       </div>
-    </Formik>
+    </div>
   );
 };
 
