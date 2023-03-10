3e644c95ffc6d1adabf4f94cc1b6341c948e01b0
Pavel <p.yudenko@gmail.com>
2023-03-10 21:21:16 +0300

    refactor: add i18n init

--- frontend/src/init.jsx
+++ frontend/src/init.jsx
@@ -1,8 +1,10 @@
 import { Provider } from 'react-redux';
+import i18next from 'i18next';
+import { I18nextProvider, initReactI18next } from 'react-i18next';
 import App from './components/App.jsx';
-
 import store, { actions } from './slices/index.js';
 import contexts from './contexts/index.js';
+import resources from './locales/index.js';
 
 const init = async (socket) => {
   const { ApiContext } = contexts;
@@ -36,11 +38,23 @@ const init = async (socket) => {
     const { id, name } = payload;
     store.dispatch(actions.renameChannel({ channelId: id, channelName: name }));
   });
+
+  const i18n = i18next.createInstance();
+
+  await i18n
+    .use(initReactI18next)
+    .init({
+      resources,
+      fallbackLng: 'ru',
+    });
+
   return (
     <Provider store={store}>
-      <ApiContext.Provider value={api}>
-        <App />
-      </ApiContext.Provider>
+      <I18nextProvider i18n={i18n}>
+        <ApiContext.Provider value={api}>
+          <App />
+        </ApiContext.Provider>
+      </I18nextProvider>
     </Provider>
   );
 };
