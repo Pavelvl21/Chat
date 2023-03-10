236ad6191be26e5369460e9e400a2fa1b9aca72a
Pavel <p.yudenko@gmail.com>
2023-03-10 22:51:32 +0300

    refactor: set signupPage locales

--- frontend/src/components/SignupPage.jsx
+++ frontend/src/components/SignupPage.jsx
@@ -4,14 +4,15 @@ import axios from 'axios';
 import { useNavigate } from 'react-router-dom';
 import { useFormik } from 'formik';
 import { Form, Button } from 'react-bootstrap';
+import { useTranslation } from 'react-i18next';
 import * as yup from 'yup';
-
 import useHook from '../hooks/index.js';
 import img from '../assets/SignupPage.png';
 
 const { useAuth } = useHook;
 
 const SignupPage = () => {
+  const { t } = useTranslation();
   const navigate = useNavigate();
   const auth = useAuth();
   const formik = useFormik({
@@ -21,9 +22,18 @@ const SignupPage = () => {
       confirmPassword: '',
     },
     validationSchema: yup.object().shape({
-      username: yup.string().min(3).max(20).required(),
-      password: yup.string().min(6).required(),
-      confirmPassword: yup.string().oneOf([yup.ref('password')]),
+      username: yup
+        .string()
+        .min(3, 'signup.lengthLimit')
+        .max(20, 'signup.lengthLimit')
+        .required('signup.required'),
+      password: yup
+        .string()
+        .min(6, 'signup.passLengthLimit')
+        .required('signup.required'),
+      confirmPassword: yup
+        .string()
+        .oneOf([yup.ref('password')], t('signup.matches')),
     }),
     onSubmit: async (values) => {
       try {
@@ -52,50 +62,50 @@ const SignupPage = () => {
               <div>
                 <img
                   src={img}
-                  alt="Registration"
+                  alt={t('signup.header')}
                 />
               </div>
               <Form onSubmit={formik.handleSubmit} className="w-50">
-                <h1 className="text-center mb-4">Регистрация</h1>
+                <h1 className="text-center mb-4">{t('signup.header')}</h1>
                 <Form.Group className="form-floating mb-3">
                   <Form.Control
                     type="text"
                     onChange={formik.handleChange}
                     value={formik.values.username}
-                    placeholder="Имя пользователя"
+                    placeholder={t('signup.lengthLimit')}
                     name="username"
                     id="username"
                     autoComplete="new-username"
                     required
                     ref={inputRef}
                   />
-                  <Form.Label htmlFor="username">Имя пользователя</Form.Label>
+                  <Form.Label htmlFor="username">{t('signup.username')}</Form.Label>
                 </Form.Group>
                 <Form.Group className="form-floating mb-3">
                   <Form.Control
                     type="password"
                     onChange={formik.handleChange}
                     value={formik.values.password}
-                    placeholder="Пароль"
+                    placeholder={t('signup.passLengthLimit')}
                     name="password"
                     autoComplete="new-password"
                     required
                   />
-                  <Form.Label htmlFor="password">Пароль</Form.Label>
+                  <Form.Label htmlFor="password">{t('signup.password')}</Form.Label>
                 </Form.Group>
                 <Form.Group className="form-floating mb-3">
                   <Form.Control
                     type="password"
                     onChange={formik.handleChange}
                     value={formik.values.confirmPassword}
-                    placeholder="Подтвердите пароль"
+                    placeholder={t('signup.matches')}
                     name="confirmPassword"
                     autoComplete="new-password"
                     required
                   />
-                  <Form.Label htmlFor="confirmPassword">Подтвердите пароль</Form.Label>
+                  <Form.Label htmlFor="confirmPassword">{t('signup.confirm')}</Form.Label>
                 </Form.Group>
-                <Button type="submit" variant="outline-primary" className="w-100">Зарегистрироваться</Button>
+                <Button type="submit" variant="outline-primary" className="w-100">{t('signup.submit')}</Button>
               </Form>
             </div>
           </div>
